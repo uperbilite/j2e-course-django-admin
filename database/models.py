e6ec22ef9bec5552e4c2c1b3468f5445aa4dc908
uperbilite <uperbilite@gmail.com>
2022-12-04 21:32:18 +0800

    create models for database table book, user and item.

--- database/models.py
+++ database/models.py
@@ -1,3 +1,39 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    discription = models.CharField(max_length=255, blank=True, null=True)
+    cover = models.CharField(max_length=255, blank=True, null=True)
+    price = models.CharField(max_length=255, blank=True, null=True)
+    stock = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'book'
+
+
+class Item(models.Model):
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    book_id = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'item'
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    balance = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'

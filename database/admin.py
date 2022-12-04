cedd3e7136f2b08013d1703d34ad0170629179c2
uperbilite <uperbilite@gmail.com>
2022-12-04 21:37:37 +0800

    register these models so that admin page can manipulate it.

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+from .models import User
+from .models import Item
+
+admin.site.register(Book)
+admin.site.register(User)
+admin.site.register(Item)

77cb211419bb4bf8e3ec28ea12020d660f6e16c2
uperbilite <uperbilite@gmail.com>
2022-12-04 21:11:38 +0800

    set database information

--- j2e_course_django_admin/settings.py
+++ j2e_course_django_admin/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'j2e_course_django_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookstore',
+        'USER': 'uperbilite',
+        'PASSWORD': 'sm200275',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/shanghai'
 
 USE_I18N = True
 
